8e865d604fb75d31c3ea661da08cb0cb343b937f
MaxRytter <maxrytter97@hotmail.com>
2025-02-18 14:21:36 +0100

    nyt

---
+++ src/main/java/com/example/hellogit3/HelloGit3Application.java
@@ -0,0 +1,13 @@
+package com.example.hellogit3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGit3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGit3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellogit3/HelloGit3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogit3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGit3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
